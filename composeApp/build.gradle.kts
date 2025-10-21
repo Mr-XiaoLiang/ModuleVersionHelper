fc3c0b3747a190b1f44e49fddb106f824718ac30
lollipop <liujian594@hotmail.com>
2025-10-21 23:27:37 +0800

    feat: 重构项目，跑起来啦

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -21,6 +21,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation("org.jetbrains.androidx.navigation:navigation-compose:2.9.1")
         }
 //        commonTest.dependencies {
 //            implementation(libs.kotlin.test)
@@ -35,7 +36,6 @@ kotlin {
             implementation("org.eclipse.jgit:org.eclipse.jgit.ssh.jsch:7.4.0.202509020913-r")
             // https://mvnrepository.com/artifact/org.json/json
             implementation("org.json:json:20250517")
-            implementation(libs.androidx.navigation.compose.jvmstubs)
             implementation(libs.androidx.runtime.saveable.desktop)
         }
     }
--- composeApp/src/jvmMain/kotlin/com/lollipop/mvh/App.kt
+++ composeApp/src/jvmMain/kotlin/com/lollipop/mvh/App.kt
@@ -1,79 +1,104 @@
 package com.lollipop.mvh
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Commit
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Output
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import org.jetbrains.compose.ui.tooling.preview.Preview
 import com.lollipop.mvh.ui.Destination
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-@Preview
 fun App() {
     MaterialTheme {
         val navController = rememberNavController()
+
         val startDestination = Destination.GIT_STATE
         var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
 
         Scaffold(
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
+                .background(MaterialTheme.colors.background)
                 .safeContentPadding()
                 .fillMaxSize(),
         ) { contentPadding ->
-
-            NavigationRail(modifier = Modifier.padding(contentPadding)) {
-                Destination.entries.forEachIndexed { index, destination ->
-                    NavigationRailItem(
-                        selected = selectedDestination == index,
-                        onClick = {
-                            navController.navigate(route = destination.name)
-                            selectedDestination = index
-                        },
-                        icon = {
-                            Icon(
-                                destination.icon,
-                                contentDescription = destination.label
-                            )
-                        },
-                        label = { Text(destination.label) }
-                    )
+            Row {
+                NavigationRail(modifier = Modifier.padding(contentPadding)) {
+                    Destination.entries.forEachIndexed { index, destination ->
+                        NavigationRailItem(
+                            selected = selectedDestination == index,
+                            onClick = {
+                                navController.navigate(route = destination.name)
+                                selectedDestination = index
+                            },
+                            icon = {
+                                Icon(
+                                    getDestinationIcon(destination),
+                                    contentDescription = destination.label
+                                )
+                            },
+                            label = { Text(destination.label) }
+                        )
+                    }
                 }
+                AppNavHost(navController, startDestination)
             }
-            AppNavHost(navController, startDestination)
-//            PrimaryTabRow(selectedTabIndex = selectedDestination, modifier = Modifier.padding(contentPadding)) {
-//                Destination.entries.forEachIndexed { index, destination ->
-//                    Tab(
-//                        selected = selectedDestination == index,
-//                        onClick = {
-//                            navController.navigate(route = destination.name)
-//                            selectedDestination = index
-//                        },
-//                        text = {
-//                            Text(
-//                                text = destination.label,
-//                                maxLines = 2,
-//                                overflow = TextOverflow.Ellipsis
-//                            )
-//                        }
-//                    )
-//                }
-//            }
-//            AppNavHost(navController, startDestination)
+        }
+    }
+}
+
+@Composable
+private fun AppNavHost(
+    navController: NavHostController,
+    startDestination: Destination
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination.name
+    ) {
+        composable(Destination.GIT_STATE.name) {
+            Box(Modifier.fillMaxSize().background(Color.Red)) {
+                Text(text = Destination.GIT_STATE.label)
+            }
+        }
+        composable(Destination.REPOSITORY_LIST.name) {
+            Box(Modifier.fillMaxSize().background(Color.Blue)) {
+                Text(text = Destination.REPOSITORY_LIST.label)
+            }
+        }
+        composable(Destination.ADD_REPOSITORY.name) {
+            Box(Modifier.fillMaxSize().background(Color.Black))
+
+        }
+        composable(Destination.VERSION_COLUMN_MANAGER.name) {
+            Box(Modifier.fillMaxSize().background(Color.Yellow))
+        }
+        composable(Destination.ADD_VERSION_COLUMN.name) {
+            Box(Modifier.fillMaxSize().background(Color.Cyan))
+        }
+        composable(Destination.MODULE_INFO_OUTPUT.name) {
+            Box(Modifier.fillMaxSize().background(Color.Magenta))
         }
     }
 }
